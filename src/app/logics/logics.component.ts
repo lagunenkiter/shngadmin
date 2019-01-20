6291bcb543cdefd7c024db7d3d066a0a4d2f18fd
Marc René Frieß <rene.friess@gmx.de>
2019-01-20 18:13:34 +0100

    Admin module: added new logics section

--- src/app/logics/logics.component.ts
+++ src/app/logics/logics.component.ts
@@ -8,6 +8,7 @@ import { BsModalService} from 'ngx-bootstrap/modal';
 import {LogicsApiService} from '../common/services/logics-api.service';
 import {LogicsinfoType} from '../common/models/logics-info';
 import {OlddataService} from '../common/services/olddata.service';
+import {Log} from '@angular/core/testing/src/logger';
 
 @Component({
   selector: 'app-logics',
@@ -18,6 +19,7 @@ import {OlddataService} from '../common/services/olddata.service';
 export class LogicsComponent implements OnInit {
 
   logicsinfo: LogicsinfoType[];
+  newlogicsinfo: LogicsinfoType[];
   constructor(private http: HttpClient, private dataService: LogicsApiService, private modalService: BsModalService) {
   }
 
@@ -28,8 +30,10 @@ export class LogicsComponent implements OnInit {
       .subscribe(
         (response) => {
             this.logicsinfo = <LogicsinfoType[]>response['logics'];
+            this.newlogicsinfo = <LogicsinfoType[]>response['logics_new'];
             this.logicsinfo.sort(function (a, b) {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0); });
-            console.log(this.logicsinfo);
+            this.newlogicsinfo.sort(function (a, b) {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0); });
+            console.log(response);
         }
       );
   }
