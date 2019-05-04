99167e9f53d36477bd85bf0c37d523e6bfc2a3d4
Marc René Frieß <rene.friess@gmx.de>
2019-05-04 18:41:12 +0200

    Fix to test case

--- cypress/integration/shng/watch_item_test.spec.js
+++ cypress/integration/shng/watch_item_test.spec.js
@@ -5,10 +5,10 @@ context('Actions', () => {
     const user = 'admin'
     const pass = '1234';
     cy.login(user, pass);
+    cy.navigateLogics();
   })
   describe('Logics: Add Watch Item Test', () => {
     it('navigates to logics parameters and enters/removes watch item', () => {
-      cy.navigateLogics();
       cy.addWatchItem('sh.avm');
       cy.removeWatchItem('sh.avm');
 
@@ -20,9 +20,8 @@ context('Actions', () => {
     })
 
     it('navigates to logics parameters and enters watch item twice', () => {
-      cy.navigateLogics();
-      cy.addWatchItem('sh.avm');
-      cy.addWatchItem('sh.avm');
+      cy.addWatchItem('avm');
+      cy.addWatchItem('avm');
       cy.contains('Item ungültig oder bereits in der Liste vorhanden, bitte Eingabe prüfen!');
     })
   })
