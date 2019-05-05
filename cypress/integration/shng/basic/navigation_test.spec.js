48319fb446ac20ef348bcdfd6abe1472bc718a24
Marc René Frieß <rene.friess@gmx.de>
2019-05-05 09:52:31 +0200

    changes to test cases

--- cypress/integration/shng/basic/navigation_test.spec.js
+++ cypress/integration/shng/basic/navigation_test.spec.js
@@ -14,7 +14,7 @@ context('Actions', () => {
   describe('Basic: Navigate to logics parameters', () => {
     it('navigates to logics parameters', () => {
       cy.get('a[href$="/logics"]').should('be.visible').click();
-      cy.wait(3000);
+      cy.wait(4000);
       cy.get('a#userlogics_wind\\.py').should('be.visible').click();
       cy.contains('Parameter').should('be.visible').click();
       cy.get('button#knx\\.weather\\.wind').should('be.visible');
@@ -23,9 +23,9 @@ context('Actions', () => {
   })
   describe('Basic: Navigate to logs', () => {
     it('navigates to log overview page', () => {
-      cy.wait(2000);
+      cy.wait(3000);
       cy.get('a[href$="/logs"]').should('be.visible').click();
-      cy.wait(2000);
+      cy.wait(3000);
       cy.get('button#reloadLogButton').should('be.visible');
       cy.contains('2019-03-17  08:42:23 ERROR').should('exist');
     })
